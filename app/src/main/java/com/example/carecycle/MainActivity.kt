ef9f6066df5bcea05d8e57fd542834953ccba628
Aaliyakhan10 <aaliyakhan4352@gmail.com>
2025-04-12 21:16:08 +0530

    2nd commit

--- app/src/main/java/com/example/carecycle/MainActivity.kt
+++ app/src/main/java/com/example/carecycle/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.carecycle
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Retrieve the NavHostFragment using supportFragmentManager
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_home) as NavHostFragment
+
+        // Get the NavController from the NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Setup BottomNavigationView with NavController
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        NavigationUI.setupWithNavController(bottomNav, navController)
     }
 }
